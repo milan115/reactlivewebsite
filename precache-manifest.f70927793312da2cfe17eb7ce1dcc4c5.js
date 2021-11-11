dacac9f7e83fa600ad9960a50065216c9ee02255
milan115 <milanlm798@gmail.com>
2021-11-12 01:52:57 +0600

    Updates

---
+++ precache-manifest.f70927793312da2cfe17eb7ce1dcc4c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478d40ae3833070332a6456d6e81e78d",
+    "url": "/reactlivewebsite/index.html"
+  },
+  {
+    "revision": "978088235a469c74511f",
+    "url": "/reactlivewebsite/static/css/main.71d76b12.chunk.css"
+  },
+  {
+    "revision": "b5f0fd39b47b16405721",
+    "url": "/reactlivewebsite/static/js/2.1a2e4dea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactlivewebsite/static/js/2.1a2e4dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978088235a469c74511f",
+    "url": "/reactlivewebsite/static/js/main.1d783658.chunk.js"
+  },
+  {
+    "revision": "d3d39ef4ceb6d5ebdf87",
+    "url": "/reactlivewebsite/static/js/runtime-main.d15608c2.js"
+  }
+]);
